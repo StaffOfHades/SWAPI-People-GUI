f38d904c336506f622b8e1518c399c6e916ce0ab
Mauricio Graciano <mau.graci@gmail.com>
2021-05-12 19:22:50 -0500

    Added redux store to project

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
     'arrow-body-style': [2, 'as-needed'],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['state'] }],
     'import/no-extraneous-dependencies': [
       'error',
       {
@@ -86,6 +87,8 @@ module.exports = {
             ],
           },
         ],
+
+        'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['state'] }],
 
         // I suggest this setting for requiring return types on functions only where useful
         '@typescript-eslint/explicit-function-return-type': [
