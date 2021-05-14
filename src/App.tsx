c32b2b7589194fb4be3d6c00dbd49bc289b9442c
Mauricio Graciano <mau.graci@gmail.com>
2021-05-14 11:16:14 -0500

    Updated App component to only have one route that is for people component

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,14 @@ import React from 'react';
 import PeopleList from './features/people/PeopleList';
 import PeoplePaginator from './features/people/PeoplePaginator';
 import PeopleSearchbar from './features/people/PeopleSearchbar';
-import logo from './assets/logo.svg';
 import './App.scss';
 
-function Home(): JSX.Element {
-  return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
-  );
-}
-
-function People(): JSX.Element {
+export function People(): JSX.Element {
   return (
     <div>
       <PeopleSearchbar />
       <PeopleList />
       <PeoplePaginator />
-    </div>
-  );
-}
-
-function Dashboard(): JSX.Element {
-  return (
-    <div>
-      <h2>Dashboard</h2>
     </div>
   );
 }
@@ -46,13 +23,7 @@ export default function App(): JSX.Element {
         <div>
           <ul>
             <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/people">People</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
+              <Link to="/">People</Link>
             </li>
           </ul>
 
@@ -60,13 +31,7 @@ export default function App(): JSX.Element {
 
           <Switch>
             <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/people">
               <People />
-            </Route>
-            <Route path="/dashboard">
-              <Dashboard />
             </Route>
           </Switch>
         </div>
